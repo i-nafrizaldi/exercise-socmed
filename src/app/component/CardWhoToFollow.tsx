54c5d4b98e40fc12f8b58df5424eea629ecd8def
i-nafrizaldi <ivannafrizaldi@gmail.com>
2024-03-21 13:16:39 +0700

    feat : layouting

--- src/app/component/CardWhoToFollow.tsx
+++ src/app/component/CardWhoToFollow.tsx
@@ -1,28 +1,52 @@
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import { UserPlus } from 'lucide-react'
-import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { UserPlus } from "lucide-react";
+import React from "react";
 
-const CardWhoToFollow :React.FC = () => {
+const CardWhoToFollow: React.FC = () => {
   return (
-    <div className='bg-gray-100 rounded-lg border border-gray-500 p-4'>
-        <h2 className='font-bold mb-5'>Who To Follow</h2>
-        <div className='flex items-center justify-between'>
-            <div className='flex items-center'>
+    <div className="p-2">
+      <h2 className="mb-5 font-bold">Who To Follow</h2>
+
+      <div className=" flex flex-col gap-4">
+        <div className="flex items-center justify-between rounded-lg border border-gray-300 bg-gray-100 p-2">
+          <div className="flex items-center gap-2">
             <Avatar className="cursor-pointer">
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-          <p>Jokowidodol</p>
-
-            </div>
-            <div>
-                <UserPlus
-                />
-            </div>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <p>Jokowidodol</p>
+          </div>
+          <div>
+            <UserPlus className="cursor-pointer" />
+          </div>
         </div>
-        
+        <div className="flex items-center justify-between rounded-lg border border-gray-300 bg-gray-100 p-2">
+          <div className="flex items-center gap-2">
+            <Avatar className="cursor-pointer">
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <p>Jokowidodol</p>
+          </div>
+          <div>
+            <UserPlus className="cursor-pointer" />
+          </div>
+        </div>
+        <div className="flex items-center justify-between rounded-lg border border-gray-300 bg-gray-100 p-2">
+          <div className="flex items-center gap-2">
+            <Avatar className="cursor-pointer">
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <p>Jokowidodol</p>
+          </div>
+          <div>
+            <UserPlus className="cursor-pointer" />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CardWhoToFollow
+export default CardWhoToFollow;
--- src/app/component/leftSection.tsx
+++ src/app/component/leftSection.tsx
@@ -1,28 +1,24 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import { Bell, HomeIcon, MessageSquareText, UserRound } from 'lucide-react';
+import { Bell, HomeIcon, MessageSquareText, UserRound } from "lucide-react";
 
 const LeftSection = () => {
   return (
     <>
       <div className="flex flex-col gap-4">
         <Button variant="ghost" className="justify-normal">
-          <HomeIcon className="mr-4 h-4 w-4"/> Home
+          <HomeIcon className="mr-4 h-4 w-4" /> Home
         </Button>
         <Button variant="ghost" className="justify-normal">
-          <Bell className="mr-4 h-4 w-4"/>Notification
+          <Bell className="mr-4 h-4 w-4" />
+          Notification
         </Button>
         <Button variant="ghost" className="justify-normal">
-          <UserRound className="mr-4 h-4 w-4"/> Profile
+          <UserRound className="mr-4 h-4 w-4" /> Profile
         </Button>
         <Button variant="ghost" className="justify-normal">
-         <MessageSquareText className="mr-4 h-4 w-4"/> Massage
+          <MessageSquareText className="mr-4 h-4 w-4" /> Massage
         </Button>
       </div>
-      <Avatar className="cursor-pointer">
-        <AvatarImage src="https://github.com/shadcn.png" />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
     </>
   );
 };
